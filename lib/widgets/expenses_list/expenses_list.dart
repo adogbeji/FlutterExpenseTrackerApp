305f48a164f074f810583a31870bc94c5ebc2a76
adogbeji <baka_1993@yahoo.co.uk>
2024-03-16 10:53:31 +0000

    Change folder structure & start building ExpensesList widgete

--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/expenses_list/expense_item.dart';
 
 class ExpensesList extends StatelessWidget {
   const ExpensesList({super.key, required this.expenses});
