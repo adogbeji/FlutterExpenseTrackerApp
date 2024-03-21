9a95e178606a86d7f204c47ef19d8bd2eeb66da7
adogbeji <baka_1993@yahoo.co.uk>
2024-03-21 11:46:57 +0000

    Add dispose() method to NewExpense state class

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -8,10 +8,12 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
-  var _enteredTitle = '';
+  final _titleController = TextEditingController();
 
-  void _saveTitleInput(String inputValue) {
-    _enteredTitle = inputValue;
+  @override
+  void dispose() {
+    _titleController.dispose();
+    super.dispose();
   }
 
   @override
@@ -19,10 +21,9 @@ class _NewExpenseState extends State<NewExpense> {
     return Padding(
       padding: const EdgeInsets.all(16),
       child: Column(children: [
-        TextField(
-          onChanged: _saveTitleInput,
+        const TextField(
           maxLength: 50,
-          decoration: const InputDecoration(
+          decoration: InputDecoration(
             label: Text('Title'),
           ),
         ),
@@ -31,9 +32,7 @@ class _NewExpenseState extends State<NewExpense> {
         Row(
           children: [
             ElevatedButton(
-              onPressed: () {
-                print(_enteredTitle);
-              },
+              onPressed: () {},
               child: const Text('Save Expense'),
             ),
           ],
