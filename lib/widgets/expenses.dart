7bae5d886e38245b754985c4ab786673e7c152b3
adogbeji <baka_1993@yahoo.co.uk>
2024-04-02 11:33:45 +0100

    Remove snack bars if multiple expenses are deleted

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -52,6 +52,7 @@ class _ExpensesState extends State<Expenses> {
     setState(() {
       _registeredExpense.remove(expense);
     });
+    ScaffoldMessenger.of(context).clearSnackBars();  // Removes all existing info messages ("snack bars")
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
         duration: const Duration(seconds: 3,),
