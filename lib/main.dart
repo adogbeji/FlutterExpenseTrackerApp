2e82f5bd0027d606291dc7e8e3f1eb03eb6b9889
adogbeji <baka_1993@yahoo.co.uk>
2024-04-04 11:44:24 +0100

    Start setting button theme

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,13 @@ void main() {
           backgroundColor: kColorScheme.onPrimaryContainer,
           foregroundColor: kColorScheme.primaryContainer,
         ),
+        cardTheme: const CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8,),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(),
+        ),
       ),
       home: const Expenses(),
     ),
