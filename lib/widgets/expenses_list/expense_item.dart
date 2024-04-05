3a96ade3186de9542f23f6c7494dd1d96af59987
adogbeji <baka_1993@yahoo.co.uk>
2024-04-05 19:22:47 +0100

    Set background colour & margin for expenses in ExpensesList widget

--- lib/widgets/expenses_list/expense_item.dart
+++ lib/widgets/expenses_list/expense_item.dart
@@ -19,8 +19,9 @@ class ExpenseItem extends StatelessWidget {
         ),
         // child: Text(expense.title,),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(expense.title),
+            Text(expense.title, style: Theme.of(context).textTheme.titleLarge,),
             const SizedBox(height: 4,),
             Row(
               children: [
