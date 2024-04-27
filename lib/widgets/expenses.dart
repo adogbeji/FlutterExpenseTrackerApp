c542461903981f09bc661bd02496be9919109275
adogbeji <baka_1993@yahoo.co.uk>
2024-04-27 20:19:14 +0100

    Unlock safe area in modal

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -33,6 +33,7 @@ class _ExpensesState extends State<Expenses> {
   void _openAddExpenseOverlay() {
     // Open modal overlay
     showModalBottomSheet(
+      useSafeArea: true,
       isScrollControlled: true,
       context: context,
       builder: (ctx) => NewExpense(onAddExpense: _addExpense),
--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -82,7 +82,7 @@ class _NewExpenseState extends State<NewExpense> {
     return SizedBox(
       child: SingleChildScrollView(
         child: Padding(
-          padding: EdgeInsets.fromLTRB(16, 48, 16, keyboardSpace + 16),
+          padding: EdgeInsets.fromLTRB(16, 16, 16, keyboardSpace + 16),
           child: Column(children: [
             TextField(
               controller: _titleController,
